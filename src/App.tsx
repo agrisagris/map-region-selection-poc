34a5c42b211338f34c66e2031e80fb8a30683c5a
agris <agris.melko@gmail.com>
2022-01-06 20:16:16 +0200

    CRA template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">map-region-selection-poc</header>
+    </div>
+  );
+}
+
+export default App;
